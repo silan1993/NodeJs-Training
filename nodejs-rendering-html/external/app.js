950049261af6ddeb692e3deb9b38b31cc55c2aee
silan <silan@silans-MacBook-Air.local>
2018-05-07 12:12:36 +0530

    added html rendring

---
+++ nodejs-rendering-html/external/app.js
@@ -0,0 +1,9 @@
+module.exports = {
+	calculate:function (a,b){
+		console.log(a+b)
+	},
+	myString : 'i am new to nodejs',
+	multiply : function (a,b){
+		console.log(a*b);
+	}
+}
---
+++ nodejs-rendering-html/index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+var app =require('./external/app');
+
+
+function onRequest(req,res){
+	res.writeHead(200,{'Content-Type' :'text/html'});
+
+	fs.readFile('./index.html',null,function(error,data){
+		if(error == null){
+			res.write(data);
+			res.end();
+		}else{
+			res.writeHead(404);
+			res.write('<h1> File Not Found </h1>')
+			res.end();
+		}
+	});
+}
+
+
+http.createServer(onRequest).listen(8080);
+
+console.log('my server is running on port no 8080');
